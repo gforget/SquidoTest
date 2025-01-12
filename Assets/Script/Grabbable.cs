e851e1d4ba96c09849b45e0771a4d0e1edf29412
gforget <gforget2@gmail.com>
2025-01-12 17:46:15 -0500

    Grab and throw action

---
+++ Assets/Script/Grabbable.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grabbable : MonoBehaviour
+{
+    private bool m_IsGrabbed = false;
+    private Transform m_Grabber = null;
+    private Rigidbody m_Rb;
+    
+    // For calculating throw velocity
+    private Vector3 m_PreviousPosition;
+    private Vector3 m_Velocity;
+    private Quaternion m_PreviousRotation; 
+    private Vector3 m_AngularVelocity;
+
+    private float m_LerpSpeed;
+    private bool m_IsInHand;
+    private void Awake()
+    {
+        m_Rb = GetComponent<Rigidbody>();
+    }
+
+    public void StartGrab(Transform controller, float lerpSpeedValue)
+    {
+        m_IsGrabbed = true;
+        m_Grabber = controller;
+        m_LerpSpeed = lerpSpeedValue;
+        
+        //Cancel physics for the lerp
+        m_Rb.useGravity = false;
+        m_Rb.isKinematic = true;
+    }
+
+    public void EndGrab(float throwForceMultiplier, float angularVelocityMultiplier)
+    {
+        m_IsGrabbed = false;
+        m_Grabber = null;
+        
+        //Reactivate physic
+        m_Rb.useGravity = true;
+        m_Rb.isKinematic = false;
+
+        if (m_IsInHand)
+        {
+            // Apply the throw force immediately before physics takes over
+            m_Rb.AddForce(m_Velocity * throwForceMultiplier, ForceMode.Impulse);
+            // Add angular velocity
+            m_Rb.angularVelocity = m_AngularVelocity * angularVelocityMultiplier;
+        }
+        
+        m_IsInHand = false;
+    }
+
+    void FixedUpdate()
+    {
+        if (m_IsGrabbed && m_Grabber != null)
+        {
+            // Move object to hand
+            if (Vector3.Distance(transform.position, m_Grabber.position) < 0.1f)
+            {
+                m_IsInHand = true;
+            }
+
+            if (m_IsInHand)
+            {
+                transform.position = m_Grabber.position;
+                transform.rotation = m_Grabber.rotation; 
+            }
+            else
+            {
+                transform.position = Vector3.Lerp(transform.position, m_Grabber.position, Time.fixedDeltaTime * m_LerpSpeed);
+                transform.rotation = Quaternion.Lerp(transform.rotation, m_Grabber.rotation, Time.fixedDeltaTime * m_LerpSpeed); 
+            }
+            
+            // Calculate the velocity
+            m_Velocity = (transform.position - m_PreviousPosition) / Time.fixedDeltaTime; // divide the delta time, since it is the velocity we get not the distance
+            
+            //--- Calculate angular velocity to rotate the object when it is thrown ---//
+            
+            // the difference between the current rotation and the previous rotation
+            Quaternion deltaRotation = transform.rotation * Quaternion.Inverse(m_PreviousRotation);
+
+            // angle = how many degrees the object has rotated, axis = the axis rotated around
+            deltaRotation.ToAngleAxis(out float angle, out Vector3 axis); 
+            
+            // Convert to radians, since unity uses radians
+            angle *= Mathf.Deg2Rad; 
+            
+            // Get the angular velocity
+            m_AngularVelocity = (angle * axis) / Time.fixedDeltaTime;
+            
+            //---[END] Calculate angular velocity to rotate the object when it is thrown ---//
+            
+            // record previous position and rotation
+            m_PreviousPosition = transform.position;
+            m_PreviousRotation = transform.rotation;
+        }
+    }
+}
--- Assets/Script/VRBaseController.cs
+++ Assets/Script/VRBaseController.cs
@@ -1,32 +1,50 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Serialization;
 
 public class VRBaseController : MonoBehaviour
 {
+    [SerializeField] private bool debugMode;
     [SerializeField] private InputActionReference positionAction;
     [SerializeField] private InputActionReference rotationAction;
-
-    private LineRenderer lineRenderer;
+    [SerializeField] private InputActionReference grabAction;
+    [SerializeField] protected InputActionReference thumbstickAction;
     
+    [Header("Grab and throw Settings")]
+    [SerializeField] private Color unselectColor = Color.white;
+    [SerializeField] private Color hoverColor = Color.green;
+    [SerializeField] private float grabRange = 100.0f;
+    [SerializeField] private float throwForceMultiplier = 5.0f;
+    [SerializeField] private float angularVelocityMultiplier = 1.0f;
+    [SerializeField] private float lerpSpeed = 10f;
+    
+    private LineRenderer m_LineRenderer;
+    private Grabbable m_CurrentHoveredGrabbedObject = null;
+    private Grabbable m_CurrentGrabbedObject = null;
+     
     private void Awake()
     {
-        lineRenderer = GetComponent<LineRenderer>();
-        lineRenderer.positionCount = 2;
+        m_LineRenderer = GetComponent<LineRenderer>();
+        m_LineRenderer.positionCount = 2;
     }
     
     private void OnEnable()
     {
         positionAction.action.Enable();
         rotationAction.action.Enable();
+        grabAction.action.Enable();
+        thumbstickAction.action.Enable();
     }
 
     private void OnDisable()
     {
         positionAction.action.Disable();
         rotationAction.action.Disable();
+        grabAction.action.Disable();
+        thumbstickAction.action.Disable();
     }
 
-    private void Update()
+    protected void Update()
     {
         // Update position
         Vector3 position = positionAction.action.ReadValue<Vector3>();
@@ -37,9 +55,54 @@ public class VRBaseController : MonoBehaviour
         
         // Apply correction for different coordinate systems
         transform.localRotation = rotation * Quaternion.Euler(65.0f, 0f, 0f);
+
+        if (debugMode)
+        {
+            // Update LineRenderer
+            m_LineRenderer.SetPosition(0, transform.position);
+            m_LineRenderer.SetPosition(1, transform.position + transform.forward * grabRange);
+        }
         
-        // Update LineRenderer
-        lineRenderer.SetPosition(0, transform.position);
-        lineRenderer.SetPosition(1, transform.position + transform.forward * 100);
+        // raycast in front of the controller
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, out hit, grabRange))
+        {
+            Grabbable grabbable = hit.collider.GetComponent<Grabbable>();
+            if (grabbable != null)
+            {
+                grabbable.GetComponent<Renderer>().material.color = hoverColor;
+                m_CurrentHoveredGrabbedObject = grabbable;
+                
+                if (grabAction.action.ReadValue<float>() > 0.5f && m_CurrentGrabbedObject == null)
+                {
+                    m_CurrentGrabbedObject = grabbable;
+                    grabbable.StartGrab(transform,lerpSpeed );
+                }
+            }
+            else
+            {
+                UnhoverGrabbable();
+            }
+        }
+        else
+        {
+            UnhoverGrabbable();
+        }
+
+        // Release object if grab button is released
+        if (grabAction.action.ReadValue<float>() <= 0.5f && m_CurrentGrabbedObject != null)
+        {
+            m_CurrentGrabbedObject.EndGrab(throwForceMultiplier, angularVelocityMultiplier);
+            m_CurrentGrabbedObject = null;
+        }
+    }
+
+    private void UnhoverGrabbable()
+    {
+        if (m_CurrentHoveredGrabbedObject != null && m_CurrentGrabbedObject == null)
+        {
+            m_CurrentHoveredGrabbedObject.GetComponent<Renderer>().material.color = unselectColor;
+            m_CurrentHoveredGrabbedObject = null;
+        }
     }
 }
