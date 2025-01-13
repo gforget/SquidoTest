053a708028c350d5eae3bb60f4f1e8b04c5e8aa4
gforget <gforget2@gmail.com>
2025-01-12 19:41:47 -0500

    Verify that the ball is moving downward when reaching the trigger - added more hoop for testing

--- Assets/Script/BasketGoalTrigger.cs
+++ Assets/Script/BasketGoalTrigger.cs
@@ -6,7 +6,18 @@ public class BasketGoalTrigger : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Balloon"))
         {
-            GameManager.Instance.currentScore++;
+            // Get the balloon's rigidbody to check its velocity
+            Rigidbody rb = other.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                // Calculate dot product between velocity and up vector
+                // If dot product is negative, the balloon is moving downward
+                float dotProduct = Vector3.Dot(rb.velocity, Vector3.up);
+                if (dotProduct < 0)
+                {
+                    GameManager.Instance.currentScore++;
+                }
+            }
         }
     }
 }
